23f26cebf9e6a0af12e178884bb1f1bdfcee5a85
navfal2010 <navfal808@gmail.com>
2024-03-07 10:40:03 +0500

    done

---
+++ main.js
@@ -0,0 +1,20 @@
+let FirstText = prompt('Ismingizni kiriting')
+
+let SecondText = prompt('Familiyangizni kiriting')
+
+let ThirdText = prompt('Yoshingizni kiriting')
+
+let FourthText = prompt('Yoqtirgan faningizni yozing')
+
+let FivethText = prompt('Yashaydigan tumaningizni kiriting')
+
+alert('Malumotlaringizni kiritganingiz uchun rahmat')
+
+console.log('Ism:'+FirstText);
+console.log('Familiya:'+SecondText);
+console.log('Yosh:'+ThirdText);
+console.log('Yoqtirgan fan:'+FourthText);
+console.log('Yashash manzili:'+FivethText);
+
+
+
